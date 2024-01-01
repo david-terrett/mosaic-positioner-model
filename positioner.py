29c786b5ce4384858e6de73675ae4e44fe2a9bfe
David Terrett <david.terrett@ntlworld.com>
2024-01-01 14:44:13 +0000

    Add positioner.can_reach()

--- positioner.py
+++ positioner.py
@@ -3,9 +3,11 @@
 import matplotlib.pyplot as plt
 
 from math import cos
+from math import fabs
 from math import pi
 from math import radians
 from math import sin
+from math import sqrt
 
 from geometry import move_point
 from geometry import move_polygon
@@ -18,6 +20,14 @@ class positioner(object):
 
 
     def __init__(self, position):
+        """
+        Create positioner
+
+        Parameters
+        ----------
+            position : point
+                position of positioner axis 1 in focal plane
+        """
 
         # Define the rotation axis of arm 1 for a positioner at placed at 0,0
         axis_1 = point(0.0, 0.0)
@@ -62,14 +72,52 @@ class positioner(object):
         self.theta_1_0 = 0.0
         self.theta_2_0 = pi
 
+        # Define the maximum and minimum radius the fibre can reach from the
+        # arm 1 axis
+        axis_1_to_axis_2 = sqrt((axis_1.x() - axis_2.x()) *
+                               (axis_1.x() - axis_2.x()) +
+                               ((axis_1.y() - axis_2.y()) *
+                               (axis_1.y() - axis_2.y())))
+        axis_2_to_fiber = sqrt((axis_2.x() - fiber.x()) *
+                               (axis_2.x() - fiber.x()) +
+                               ((axis_2.y() - fiber.y()) *
+                               (axis_2.y() - fiber.y())))
+        self.max_r = axis_1_to_axis_2 + axis_2_to_fiber
+        self.min_r = fabs(axis_2_to_fiber - axis_1_to_axis_2)
+
         # Park the positioner
         self.park()
 
+    def can_reach(self, p):
+        """
+        Fiber can reach point
+
+        Parameters
+        ----------
+        p : point
+            position to test
+
+        Returns
+        -------
+        : boolean
+            True if the fiber can be postioned at the specified point
+        """
+        r2 = ((p.x() - self.axis_1_0.x()) * (p.x() - self.axis_1_0.x()) +
+              (p.y() - self.axis_1_0.y()) * (p.y() - self.axis_1_0.y()))
+        return r2 < self.max_r * self.max_r and r2 > self.min_r * self.min_r
+
+
     def park(self):
+        """
+        Park the positioner
+        """
         self.pose(0.0, pi)
 
 
     def plot(self):
+        """
+        Plot the positioner outline
+        """
         plt.plot(self.arm_1.x(), self.arm_1.y(), color='black')
         plt.plot(self.arm_2.x(), self.arm_2.y(), color='black')
         plt.plot(self.axis_1_0.x(), self.axis_1_0.y(), '+')
@@ -78,6 +126,16 @@ class positioner(object):
 
 
     def pose(self, theta_1, theta_2):
+        """
+        Set the axis positions
+
+        Parmeters
+        ---------
+        theta_1 : float
+            Angle (relative to y axis) of axis 1 (radians)
+        theta_2 : float
+            Angle (relative to y axis) of axis 2 (radians)
+        """
 
         # Rotate arm 1
         c = cos(theta_1 - self.theta_1_0)
