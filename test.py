4e9f01d0e3d1505a32a3f9377b6a75015f8eccb7
David Terrett <david.terrett@ntlworld.com>
2024-09-09 12:52:42 +0100

    Create simple test script

---
+++ test.py
@@ -0,0 +1,10 @@
+from matplotlib import pyplot as plt
+
+from positioner_model import focal_plane
+
+fp = focal_plane()
+fp.add_random_targets(density=4, ir=True)
+fp.add_random_targets(density=4, vis=True)
+fp.simple_allocator()
+fp.plot()
+plt.show()
